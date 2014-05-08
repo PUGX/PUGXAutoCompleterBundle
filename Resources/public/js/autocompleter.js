df6764fb98e7fb92f989d8c7fb73fc198eec5249
Massimiliano Arione <garakkio@gmail.com>
2014-05-08 15:49:18 +0200

    jslint autocompleter.js

--- Resources/public/js/autocompleter.js
+++ Resources/public/js/autocompleter.js
@@ -1,27 +1,27 @@
-(function($) {
-    $.fn.autocompleter = function(options) {
+(function ($) {
+    'use strict';
+    $.fn.autocompleter = function (options) {
         var settings = {
             url_list: '',
             url_get:  ''
         };
-        return this.each(function() {
+        return this.each(function () {
             if (options) {
                 $.extend(settings, options);
             }
-            var $this = $(this);
-            var $fakeInput = $('<input type="text" name="fake' + $this.attr('name') + '">');
+            var $this = $(this), $fakeInput = $('<input type="text" name="fake' + $this.attr('name') + '">');
             $this.hide().after($fakeInput);
             $fakeInput.autocomplete({
                 source: settings.url_list,
-                select: function(event, ui) {
+                select: function (event, ui) {
                     $this.val(ui.item.id);
                 }
             });
             if ($this.val() != '') {
                 $.ajax({
                     url:     settings.url_get + $this.val(),
-                    success: function(name) {
-                        $fakeInput.val(name)
+                    success: function (name) {
+                        $fakeInput.val(name);
                     }
                 });
             }
