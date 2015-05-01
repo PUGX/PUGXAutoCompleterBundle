f2e3e6878bb50c0c6599c58f68b14705c93143f6
Philip van Til <philip@fiendo.nl>
2015-05-01 21:04:28 +0200

    Clone the original input to preserve useful attributes

--- Resources/public/js/autocompleter-jqueryui.js
+++ Resources/public/js/autocompleter-jqueryui.js
@@ -9,7 +9,9 @@
             if (options) {
                 $.extend(settings, options);
             }
-            var $this = $(this), $fakeInput = $('<input type="text" name="fake' + $this.attr('name') + '">');
+            var $this = $(this), $fakeInput = $this.clone();
+            $fakeInput.attr('id', 'fake_' + $fakeInput.attr('id'));
+            $fakeInput.attr('name', 'fake_' + $fakeInput.attr('name'));
             $this.hide().after($fakeInput);
             $fakeInput.autocomplete({
                 source: settings.url_list,
