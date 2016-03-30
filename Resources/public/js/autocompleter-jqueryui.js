e3f5febaf3bfbbcb6a03b00a5791156804617f5f
d_hound <james.whiteman@gmail.com>
2016-03-31 12:22:36 +1300

    jquery search option

--- Resources/public/js/autocompleter-jqueryui.js
+++ Resources/public/js/autocompleter-jqueryui.js
@@ -4,9 +4,11 @@
         var settings = {
             url_list: '',
             url_get:  '',
-            min_length: 2,
+            url_search: '',
+            min_length: 1,
             on_select_callback: null
         };
+
         return this.each(function () {
             if (options) {
                 $.extend(settings, options);
@@ -15,20 +17,34 @@
             $fakeInput.attr('id', 'fake_' + $fakeInput.attr('id'));
             $fakeInput.attr('name', 'fake_' + $fakeInput.attr('name'));
             $this.hide().after($fakeInput);
+
             $fakeInput.autocomplete({
                 source: settings.url_list,
+                search: function(event,ui) {
+                    $('#'+ $this.attr('id')).val($fakeInput.val()) ;
+                },
                 select: function (event, ui) {
                     $this.val(ui.item.id);
+                    if($.isNumeric($this.val())){
+                        window.location.href = settings.url_get + $this.val();
+                    }
+                    else
+                    {
+                        window.location.href = settings.url_list + $this.val();
+                    }
                     if (settings.on_select_callback) {
                         settings.on_select_callback($this);
                     }
                 },
+
                 minLength: settings.min_length
             });
-            if ($this.val() !== '') {
+            if ($this.val() !== '' && $.isNumeric($this.val())) {
                 $.ajax({
+
                     url:     settings.url_get + $this.val(),
                     success: function (name) {
+
                         $fakeInput.val(name);
                     }
                 });
