6009154187044ddc882bfdcdeaf0b95e1bdfff53
Nikita Loges <git@nikita-loges.de>
2016-10-25 19:54:36 +0200

    load config automatically

--- DependencyInjection/PUGXAutocompleterExtension.php
+++ DependencyInjection/PUGXAutocompleterExtension.php
@@ -5,7 +5,7 @@ namespace PUGX\AutocompleterBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -14,16 +14,13 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * @codeCoverageIgnore
  */
-class PUGXAutocompleterExtension extends Extension
+class PUGXAutocompleterExtension extends ConfigurableExtension
 {
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function loadInternal(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
