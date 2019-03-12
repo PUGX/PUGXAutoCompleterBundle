7c1d007bb24cbbf203d4a086084cd04f58d3d321
Massimiliano Arione <garakkio@gmail.com>
2019-03-12 15:34:30 +0100

    remove deprecation. Fix #57

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -6,18 +6,14 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * This is the class that validates and merges configuration from config files.
- *
- * To learn more see {@link http://symfony.com/doc/current/bundles/extension.html#manually-registering-an-extension-class}
- *
  * @codeCoverageIgnore
  */
 class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('pugx_autocompleter');
+        $treeBuilder = new TreeBuilder('pugx_autocompleter');
+        $rootNode = \method_exists($treeBuilder, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('pugx_autocompleter');
 
         return $treeBuilder;
     }
