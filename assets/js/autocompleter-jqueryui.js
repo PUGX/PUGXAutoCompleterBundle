ff9ecf96c0e63ec9c0ead9cfe8f53e71532b3fe6
Dewi GLEAU <dewi.gleau@free.fr>
2021-07-24 16:38:20 +0200

    Put directly the id in the true input

--- assets/js/autocompleter-jqueryui.js
+++ assets/js/autocompleter-jqueryui.js
@@ -1,7 +1,7 @@
 (function ($) {
     'use strict';
     $.fn.autocompleter = function (options) {
-        var settings = {
+        let settings = {
             url_list: '',
             url_get:  '',
             min_length: 2,
@@ -11,7 +11,8 @@
             if (options) {
                 $.extend(settings, options);
             }
-            var $this = $(this), $fakeInput = $this.clone();
+
+            let $this = $(this), $fakeInput = $this.clone();
             $fakeInput.attr('id', 'fake_' + $fakeInput.attr('id'));
             $fakeInput.attr('name', 'fake_' + $fakeInput.attr('name'));
             $this.hide().after($fakeInput);
@@ -19,7 +20,8 @@
                 source: settings.url_list,
                 select: function (event, ui) {
                     event.preventDefault();
-                    $this.val(ui.item.value);
+
+                    $this.val(ui.item.id);
                     $(this).val(ui.item.label);
                     if (settings.on_select_callback) {
                         settings.on_select_callback($this, event, ui);
@@ -27,14 +29,6 @@
                 },
                 minLength: settings.min_length
             });
-            if ($this.val() !== '') {
-                $.ajax({
-                    url: (settings.url_get.substring(-1) === '/' ? settings.url_get : settings.url_get + '/') + $this.attr('value'),
-                    success: function (name) {
-                        $fakeInput.val(name);
-                    }
-                });
-            }
         });
     };
 })(jQuery);
